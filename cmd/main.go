5159646b0f8aec657f9a3d785f64b9da103e2474
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2022-12-08 14:29:11 -0500

    adding views folder and installing html template from Go Fiber

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/JessicaRodriguesFelix/crud-api-golang-postgres/database"
+	"github.com/gofiber/template/html"
 )
 
 func main() {
 	database.ConnectDb()
-	app := fiber.New()
+
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config {
+		Views: engine,
+	})
 
 	setupRoutes(app)
 

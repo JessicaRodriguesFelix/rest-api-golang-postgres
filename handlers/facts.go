346a6ae9bbf60310ce6cd1a50ed4680335b00192
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2022-12-08 16:30:15 -0500

    adding a new form fact form

--- handlers/facts.go
+++ handlers/facts.go
@@ -8,10 +8,20 @@ import (
 
 func ListFacts(c *fiber.Ctx) error {
 	facts:= []models.Fact{}
+
 	database.DB.Db.Find(&facts)
+
 	return c.Render("index", fiber.Map{
 		"Title": "Div Golang API",
 		"Subtitle": "Questions and Answers for funtimes with friends!",
+		"Facts": facts,
+	})
+}
+
+func NewFactView(c *fiber.Ctx) error {
+	return c.Render("new", fiber.Map{
+		"Title" : "New Fact",
+		"Subtitle": "Add a cool fact here!",
 	})
 }
 
